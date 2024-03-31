f184dbef2fe43d4b1efa28f3be809559324cd7a2
nullwyrm <72226867+nullwyrm@users.noreply.github.com>
2024-03-31 13:37:02 +0100

    Added base infrastructure for Note Source

---
+++ static/modules/backend_note_source.js
@@ -0,0 +1,30 @@
+moduleData = {
+    "name":"default_server_note_source", // Unique Identifier
+    "version":"1.0", // Used in case module sources have multiple copies (todo)
+    "author":"Group 15 Team", // No direct influence currently, but likely good to have
+    "isModuleSource":false,
+    "isNoteSource":true,
+    "sourcePriority":100 // Used to resolve cases where multiple sources have a file
+}
+
+export class Module {
+    constructor(Handler) {
+        
+    }
+    
+    async getNoteList() { // obtains list of notes available from this source
+    }
+    
+    async postNote(name, data) { // send data to store as new note
+    }
+    
+    async getNote(name) { // retrieve given note
+    }
+    
+    async patchNote(name, data) { // unsure how this differs from post
+        return await postNote(name, data); // Potentially a placeholder, though I'm not certain what the difference would actually be
+    }
+    
+    async deleteNote(name) { // deletes note with given name
+    } 
+}
