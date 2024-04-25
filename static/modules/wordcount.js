8c87e833a207e9e91bcaed0191aa3e23f5829896
Junyuan Chen <junyuan.chen@sjc.ox.ac.uk>
2024-04-25 21:36:42 +0000

    Replace tabs with spaces

--- static/modules/wordcount.js
+++ static/modules/wordcount.js
@@ -9,13 +9,13 @@ counter.style.marginRight = "1rem";
 counter.style.color = "var(--alma-grey)";
 
 function updateCounter() {
-	const text = $.editor.quill.getText();
-	const words = text.split(/\s+/).filter(str => /\w+/.test(str));
-	if (words.length === 1) {
-		counter.innerText = "1 word";
-	} else {
-		counter.innerText = `${words.length} words`;
-	}
+    const text = $.editor.quill.getText();
+    const words = text.split(/\s+/).filter(str => /\w+/.test(str));
+    if (words.length === 1) {
+        counter.innerText = "1 word";
+    } else {
+        counter.innerText = `${words.length} words`;
+    }
 }
 
 document.querySelector("body > main .ql-toolbar").append(counter);
