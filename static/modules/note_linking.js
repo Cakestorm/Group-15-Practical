ba112944859b501e83dc331c5e812bcf4f0ba916
LukangOwen <68473517+LukangOwen@users.noreply.github.com>
2024-04-26 00:16:47 +0800

    make sure the linked notes button works

--- static/modules/note_linking.js
+++ static/modules/note_linking.js
@@ -23,17 +23,18 @@ export class Module {
         //TODO: pass this to more specific functions that automatically finds relevant links, based on keywords or semantic similarity etc.
         //TODO: user should be able to customise the linked notes as well?
         
-        let data = await this.handler.getNoteList();
+        let pth_list = await this.handler.getNoteList();
+        // TODO: pth_list = get_linked_notes(current_name, list_of_notes)
 
         //Modify the attributes of a saved note
         let name = document.getElementById("name").value;
-        this.handler.modifyNote("default_server_note_source", name, JSON.stringify({"links":data}))
+        this.handler.modifyNote("default_server_note_source", name, JSON.stringify({"links":pth_list}))
 
         //Display the names of linked notes on the webpage (TODO: better display paths to those notes)
         //TODO: Displaying the linked notes should also be done when a note is loaded.
         let display_str = ""
-        for(let x in data) {
-            display_str += ('>> '+ data[x] + '.note\n')
+        for(let x in pth_list) {
+            display_str += ('>> '+ pth_list[x] + '.note\n')
         }
         document.getElementById("linked_notes").value = display_str;
     }
